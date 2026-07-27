6cabe5cf9a69547d897408a77d9951b16194a132
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-27 07:39:16 -0300

    Implementação dos atributos name, price, quantity

---
+++ src/application/Loja.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Product;
+
+import java.util.Scanner;
+
+public class Loja {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        product.name = sc.nextLine();
+        System.out.println("Price: ");
+        product.price = sc.nextDouble();
+        System.out.println("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println(product.name + ", "
+                + product.price + ", "
+                + product.quantity);
+    }
+}
