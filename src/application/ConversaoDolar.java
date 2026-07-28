7ebabdf57fa6d193450f682329ae0b2173230ba5
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-28 08:15:52 -0300

    Conversao do dólar

---
+++ src/application/ConversaoDolar.java
@@ -0,0 +1,22 @@
+package application;
+
+import entities.CurrencyConverter;
+
+import java.util.Scanner;
+
+public class ConversaoDolar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the dollar price? ");
+        double dollar = sc.nextDouble();
+        System.out.println("How many dollars will be bought? ");
+        double money = sc.nextDouble();
+
+        double result = CurrencyConverter.taxDolar(dollar, money);
+
+        System.out.printf("Amount to be paid in reais = %.2f%n", result);
+
+    }
+}
---
+++ src/entities/CurrencyConverter.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class CurrencyConverter {
+
+    public static final double IOF = 0.06;
+
+    public static double taxDolar (double dollarPrice, double money) {
+        double total = dollarPrice * money;
+        double tax = total * IOF;
+        return total + tax;
+    }
+}
