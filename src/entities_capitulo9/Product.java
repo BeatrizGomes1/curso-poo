618d9b8cd333d4e15d0107f658a1a3ff17e5f95f
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-28 09:37:43 -0300

    Implementando encapsulamento

--- src/entities_capitulo9/Product.java
+++ src/entities_capitulo9/Product.java
@@ -2,9 +2,9 @@ package entities_capitulo9;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(String name, double price, int quantity) {
         this.name = name;
@@ -17,18 +17,35 @@ public class Product {
         this.price = price;
     }
 
-    public double totalValueInStock() {
+    public String getName() {
+        return name;
+    }
 
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice (double price) {
+        this.price = price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public double totalValueInStock() {
         return price * quantity;
     }
 
     public void addProducts(int quantity) {
-
         this.quantity += quantity; //this acessa atributo da classe
     }
 
     public void removeProducts(int quantity){
-
         this.quantity -= quantity;
     }
 
