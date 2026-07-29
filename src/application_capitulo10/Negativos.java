715bbee2bb632a896ce5c01a2939b52da6a2324d
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-29 13:40:14 -0300

    Números negativos com vetores

---
+++ src/application_capitulo10/Negativos.java
@@ -0,0 +1,29 @@
+package application_capitulo10;
+
+import java.util.Scanner;
+
+public class Negativos {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos números você vai digitar? ");
+        int n = sc.nextInt();
+        int[] vect = new int[n];
+
+        for (int i = 0; i < vect.length; i++){
+            sc.nextLine();
+            System.out.println("Digite um número: ");
+            vect[i] = sc.nextInt();
+        }
+
+        System.out.println("NÚMEROS NEGATIVOS: ");
+        for (int i = 0; i < vect.length; i++){
+            if (vect[i] < 0){
+                System.out.println(vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
