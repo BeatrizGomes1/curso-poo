b6cfc8d504ecbc6637104307b7ccfdd0a60aaeac
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-29 16:27:35 -0300

    Elementos do vetor abaixo da média

---
+++ src/application_capitulo10/Abaixo_Da_Media.java
@@ -0,0 +1,38 @@
+package application_capitulo10;
+
+import java.util.Scanner;
+
+public class Abaixo_Da_Media {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double somaVetor = 0.0;
+        double media = 0.0;
+
+        System.out.println("Quantos elementos vai ter o vetor? ");
+        int n = sc.nextInt();
+        double [] vect = new double [n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.println("Digite um número: ");
+            vect[i] = sc.nextDouble();
+
+            somaVetor += vect[i];
+        }
+
+        media = somaVetor / vect.length;
+
+        System.out.println("MÉDIA DO VETOR = " + media);
+
+        System.out.println("ELEMENTOS ABAIXO DA MÉDIA:");
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] <= 10) {
+                System.out.println(vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+
+}
