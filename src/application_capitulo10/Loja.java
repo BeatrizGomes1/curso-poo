57ac5a78a84afd4dd29d8056d7e41c9c5aa882c8
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-29 13:15:26 -0300

    Loja com vetores usando length

--- src/application_capitulo10/Loja.java
+++ src/application_capitulo10/Loja.java
@@ -12,7 +12,7 @@ public class Loja {
         int n = sc.nextInt();
         Product[] vect = new Product[n];
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < vect.length; i++){
             sc.nextLine();
             String name = sc.nextLine();
             double price = sc.nextDouble();
@@ -20,10 +20,10 @@ public class Loja {
         }
 
         double sum = 0.0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < vect.length; i++) {
             sum += vect[i].getPrice();
         }
-        double avg = sum /n;
+        double avg = sum / vect.length;
 
         System.out.printf("AVERAGE PRICE = %.2f%n", avg);
 
