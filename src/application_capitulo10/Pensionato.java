674dd42f8ea353c25c7ccf9c57e012ded49bf8eb
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-30 16:00:21 -0300

    Aluguel de quartos com vetores

---
+++ src/application_capitulo10/Pensionato.java
@@ -0,0 +1,39 @@
+package application_capitulo10;
+
+import entities_capitulo10.Rent;
+
+import java.util.Scanner;
+
+public class Pensionato {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Rent[] vect = new Rent[10];
+
+        System.out.println("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            sc.nextLine();
+            System.out.println("Rent #" + i + ":");
+            System.out.println("Name: ");
+            String name = sc.nextLine();
+            System.out.println("Email: ");
+            String email = sc.nextLine();
+            System.out.println("Room: ");
+            int room = sc.nextInt();
+
+            vect[room] = new Rent(name, email);
+        }
+
+        System.out.println();
+        System.out.println("Busy rooms: ");
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] != null){
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
+        sc.close();
+    }
+}
