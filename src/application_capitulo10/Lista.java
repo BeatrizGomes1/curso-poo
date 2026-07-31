f33b5b08428871affdea303239156361c360fa91
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-31 06:43:02 -0300

    Removendo elementos da lista

--- src/application_capitulo10/Lista.java
+++ src/application_capitulo10/Lista.java
@@ -11,11 +11,31 @@ public class Lista {
 
         List<String> list = new ArrayList<>();
 
+        //Adicionando elementos a lista
         list.add("Maria");
         list.add("Alex");
         list.add("Bob");
         list.add("Anna");
 
+        //Adicionando elemento em posição já ocupada
+        list.add(2, "Marco");
+
+        //Ver tamanho da lista
+        System.out.println(list.size());
+
+        for (String x : list) {
+            System.out.println(x);
+        }
+
+        System.out.println("----------------");
+        //Remover elemento passando o nome no parametro
+        list.remove("Bob");
+
+        //Remover baseado na posição
+        list.remove(1);
+
+        //Removendo baseado no predicado
+        list.removeIf(x -> x.charAt(0) == 'M');
         for (String x : list) {
             System.out.println(x);
         }
