84b0d6869920fd436dca015173118c3f93fb8239
Beatriz <anabeatrizsantos3830@gmail.com>
2026-07-31 06:33:08 -0300

    Adicionando elementos na lista

---
+++ src/application_capitulo10/Lista.java
@@ -0,0 +1,25 @@
+package application_capitulo10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lista {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<String> list = new ArrayList<>();
+
+        list.add("Maria");
+        list.add("Alex");
+        list.add("Bob");
+        list.add("Anna");
+
+        for (String x : list) {
+            System.out.println(x);
+        }
+
+        sc.close();
+    }
+}
