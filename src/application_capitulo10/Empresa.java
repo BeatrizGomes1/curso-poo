5dfa23c7d42e17212a61c38c1678cf082429c3ab
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-01 06:38:45 -0300

    Lista

---
+++ src/application_capitulo10/Empresa.java
@@ -0,0 +1,59 @@
+package application_capitulo10;
+
+import entities_capitulo10.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Empresa {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("How many employees will be registered? ");
+        int n = sc.nextInt();
+
+        System.out.println();
+
+        for (int i = 0; i <= n; i++) {
+            sc.nextLine();
+            System.out.println("Employee #" + i + ":");
+            System.out.println("Id: ");
+            int id = sc.nextInt();
+            System.out.println("Name: ");
+            String name = sc.nextLine();
+            System.out.println("Salary: ");
+            double salary = sc.nextDouble();
+
+            Employee emp = new Employee(id, name, salary);
+            list.add(emp);
+        }
+
+        System.out.println();
+
+        System.out.println("Enter the employee id that will have salary increase : ");
+        int idIncrese = sc.nextInt();
+        Integer pos = position(list, idIncrese);
+        if (pos == null){
+            System.out.println("This id does not exits!");
+        } else {
+            System.out.println("Enter the percentage: ");
+            double percent = sc.nextDouble();
+            list.get(pos).increaseSalary(percent);
+        }
+
+        sc.close();
+    }
+
+    public static Integer position(List<Employee> list, int id) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id) {
+                return i;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/entities_capitulo10/Employee.java
@@ -0,0 +1,35 @@
+package entities_capitulo10;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void increaseSalary (double percentage){
+        salary += salary * percentage / 100.0;
+    }
+
+}
