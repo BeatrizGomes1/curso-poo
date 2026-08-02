21ddd8a679ee16e251993c2a231735c6ad15d866
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-02 09:11:54 -0300

    Exercicio de lista - Escola

---
+++ src/application_lista_de_arraylist/Escola.java
@@ -0,0 +1,73 @@
+package application_lista_de_arraylist;
+
+
+import entities_lista_de_arraylist.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Escola {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Aluno> list = new ArrayList<>();
+
+        System.out.println("Digite a quantidade de alunos: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++){
+            System.out.println("Aluno #" + (i + 1) + ":");
+            System.out.println("Matrícula: ");
+            int matricula = sc.nextInt();
+            while (verificacaoMatricula(list, matricula)){
+                System.out.println("Matrícula já registrada! Digite novamente: ");
+                matricula = sc.nextInt();
+            }
+            sc.nextLine();
+            System.out.println("Nome: ");
+            String nome = sc.nextLine();
+            System.out.println("Nota final: ");
+            double nota = sc.nextDouble();
+
+            Aluno a = new Aluno(matricula, nome, nota);
+            list.add(a);
+
+        }
+
+        System.out.println();
+
+        System.out.println("Digite a matrícula para adicionar pontos extras: ");
+        double matriculaPonto = sc.nextDouble();
+
+        Aluno a = list.stream().filter(x -> x.getMatricula() == matriculaPonto)
+                        .findFirst().orElse(null);
+
+        if (a == null) {
+            System.out.println("Matrícula inexistente!");
+        } else {
+            System.out.println("Digite a quantidade de pontos: ");
+            double pontoExtra = sc.nextDouble();
+            a.pontosExtras(pontoExtra);
+        }
+
+        System.out.println();
+
+        System.out.println("--- ALUNOS CADASTRADOS ---");
+        for (Aluno aluno : list){
+            System.out.println(aluno);
+        }
+
+        sc.close();
+    }
+
+    public static boolean verificacaoMatricula(List<Aluno> list, int matricula){
+        Aluno a = list.stream().filter(x -> x.getMatricula() == matricula)
+                  .findFirst().orElse(null);
+        return a != null;
+    }
+}
---
+++ src/entities_lista_de_arraylist/Aluno.java
@@ -0,0 +1,47 @@
+package entities_lista_de_arraylist;
+
+public class Aluno {
+
+    private int matricula;
+    private String nome;
+    private double notaFinal;
+
+    public Aluno(int matricula, String nome, double notaFinal) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.notaFinal = notaFinal;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void pontosExtras(double ponto){
+        notaFinal += ponto;
+
+        if (notaFinal > 100){
+            System.out.println("Pontos adicionados! Nota ajustada para o limite máximo de 100.0");
+            notaFinal = 100;
+        }
+    }
+
+    @Override
+    public String toString (){
+        return "Matrícula: "
+                + matricula + " | "
+                + "Nome: " + nome + " | "
+                + "Nota: " + String.format("%.2f" , notaFinal);
+    }
+}
