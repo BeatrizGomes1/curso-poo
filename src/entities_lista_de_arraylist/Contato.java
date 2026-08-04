82f9494de3ad9be71c9bf4477f9da45ac5b85202
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-04 18:41:30 -0300

    Exercicio de lista - Contato

--- src/entities_lista_de_arraylist/Contato.java
+++ src/entities_lista_de_arraylist/Contato.java
@@ -1,4 +1,39 @@
 package entities_lista_de_arraylist;
 
 public class Contato {
+
+    private int id;
+    private String nome;
+    private String numero;
+
+    public Contato(int id, String nome, String numero) {
+        this.id = id;
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Resultado -> ID: " +  id + " | " + "Nome: " + nome + " | " +  "Tel: " + numero;
+    }
 }
