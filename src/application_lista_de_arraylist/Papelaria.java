d4888a0de0f2a912d0c7981d9ce5243cde070efe
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-12 19:58:02 -0300

    Exercicio de lista - Papelaria

--- src/application_lista_de_arraylist/Papelaria.java
+++ src/application_lista_de_arraylist/Papelaria.java
@@ -13,7 +13,33 @@ public class Papelaria {
 
         List<Produto1> produto = new ArrayList<>();
 
+        int resp;
+        do {
 
+            System.out.println("--- MENU LOJA ---");
+            System.out.println("1 - Cadastrar Produto");
+            System.out.println("2 - Adicionar nova quantidade");
+            System.out.println("3 - Venda de produto");
+            System.out.println("4 - Lista de produtos");
+            System.out.println("5 - Sair");
+            System.out.println("Escolha uma opção: ");
+            resp = sc.nextInt();
+
+            if (resp == 1) {
+                System.out.println("Digite o código do produto: ");
+                int codigo = sc.nextInt();
+                System.out.println("Digite o nome do produto: ");
+                String nome = sc.nextLine();
+                System.out.println("Digite a quantidade do produto: ");
+                int quantidade = sc.nextInt();
+
+                Produto1 produto1 = new Produto1(codigo, nome, quantidade);
+                produto.add(produto1);
+            } if (resp == 2) {
+
+            }
+
+        } while (resp != 6);
 
         sc.close();
 
