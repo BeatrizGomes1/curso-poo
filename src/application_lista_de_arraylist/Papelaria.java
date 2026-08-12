3ff5aefebf64a7f6c9be301c2433c82917b6a260
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-11 21:24:56 -0300

    Exercicio de lista - Papelaria

---
+++ src/application_lista_de_arraylist/Papelaria.java
@@ -0,0 +1,25 @@
+package application_lista_de_arraylist;
+
+import entities_lista_de_arraylist.Produto1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Papelaria {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        List<Produto1> produtos = new ArrayList<>();
+
+
+
+
+
+        sc.close();
+
+    }
+
+}
