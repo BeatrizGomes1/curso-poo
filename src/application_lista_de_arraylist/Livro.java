4151e2b217e6af93722227ba1bd6d88aa9496df2
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-14 16:44:24 -0300

    Exercicio de lista - Livro

--- src/application_lista_de_arraylist/Livro.java
+++ src/application_lista_de_arraylist/Livro.java
@@ -1,6 +1,25 @@
 package application_lista_de_arraylist;
 
+import entities_lista_de_arraylist.Autor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Livro {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-}
+        List<Autor> autores = new ArrayList<>();
+
+        Autor a = new Autor (123, "Harry Potter", "JK", 45);
+        autores.add(a);
+
+
+
+        sc.close();
+
+    }
+
+    }
