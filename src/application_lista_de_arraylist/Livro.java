57a866727a9cfb791b3a3accf3829a0f43d2aa7d
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-14 16:37:07 -0300

    Exercicio de lista - Livro

---
+++ src/application_lista_de_arraylist/Livro.java
@@ -0,0 +1,6 @@
+package application_lista_de_arraylist;
+
+public class Livro {
+
+
+}
---
+++ src/entities_lista_de_arraylist/Autor.java
@@ -0,0 +1,48 @@
+package entities_lista_de_arraylist;
+
+public class Autor {
+
+    private int isbn;
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Autor(int isbn, String titulo, String autor, double preco) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
