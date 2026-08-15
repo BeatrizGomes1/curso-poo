6701f2c860a72b8132e58e571ea6950d47cd3443
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-15 07:33:11 -0300

    Matriz - Exercício de fixação

--- src/application_capitulo10/Exercicio_Matriz.java
+++ src/application_capitulo10/Exercicio_Matriz.java
@@ -14,10 +14,32 @@ public class Exercicio_Matriz {
 
         int[][] matriz = new int[linhas][colunas];
 
-        for(int i = 0; i < linhas; i++){
-            for(int j = 0; j < colunas; j++){
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
                 matriz[i][j] = sc.nextInt();
             }
+        }
+
+        System.out.println();
+
+        System.out.println("Digite o número que deseja saber a posição: ");
+        int numero = sc.nextInt();
+
+        int posicao = -1;
+        int posicao1 = -1;
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                if (matriz[i][j] == numero){
+                    posicao = i;
+                    posicao1 = j;
+                }
+            }
+        }
+
+        if (posicao != -1) {
+            System.out.println("Posição do valor " + numero + ": " + posicao + " , " + posicao1);
+        } else {
+            System.out.println("Valor não encontrado!");
         }
 
         sc.close();
