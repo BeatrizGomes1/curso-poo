0587a9096535188d505767d008857ebf42d3d03d
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-15 06:59:05 -0300

    Matriz

--- src/application_capitulo10/Matrizes.java
+++ src/application_capitulo10/Matrizes.java
@@ -12,21 +12,21 @@ public class Matrizes {
         int [][] matriz = new int[n][n];
 
         for(int i = 0; i < matriz.length; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < matriz[i].length; j++){
                 matriz[i][j] = sc.nextInt();
             }
         }
 
         System.out.println("Main diagonal: ");
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < matriz.length; i++){
             System.out.print(matriz[i][i] + " ");
         }
 
         System.out.println();
 
         int count = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
                 if (matriz[i][j] < 0){
                     count++;
                 }
