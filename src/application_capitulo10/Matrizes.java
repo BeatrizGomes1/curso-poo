2354726bf09cbc219ad227e4506755d471dccb0d
Beatriz <anabeatrizsantos3830@gmail.com>
2026-08-15 06:55:17 -0300

    Matriz

--- src/application_capitulo10/Matrizes.java
+++ src/application_capitulo10/Matrizes.java
@@ -22,6 +22,18 @@ public class Matrizes {
             System.out.print(matriz[i][i] + " ");
         }
 
+        System.out.println();
+
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if (matriz[i][j] < 0){
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Negative numbers: " +  count);
 
         sc.close();
     }
